da2985e1dee818a04e94f846c60039b367549dcd
RubyDragoon7197 <Andrespedrozab2004@gmail.com>
2025-11-23 20:25:04 +0000

    color

--- screens/SplashScreen.js
+++ screens/SplashScreen.js
@@ -1,12 +1,17 @@
 // screens/SplashScreen.js
 import React from 'react';
-import { View, Image, ActivityIndicator, StyleSheet } from 'react-native';
+import { View, Image, Text, ActivityIndicator, StyleSheet } from 'react-native';
 
 export default function SplashScreen() {
   return (
     <View style={styles.container}>
       {/* Logo centrado */}
       <Image source={require('../assets/logo.png')} style={styles.logo} />
+      
+      {/* Texto debajo del logo */}
+      <Text style={styles.title}>Instagram Replica</Text>
+      
+      {/* Loader */}
       <ActivityIndicator size="large" color="tomato" />
     </View>
   );
@@ -22,7 +27,13 @@ const styles = StyleSheet.create({
   logo: { 
     width: 150, 
     height: 150, 
-    marginBottom: 20, 
+    marginBottom: 10, 
     resizeMode: 'contain' 
+  },
+  title: { 
+    fontSize: 20, 
+    fontWeight: 'bold', 
+    marginBottom: 20, 
+    color: '#E1306C' // 👈 color estilo Instagram (rosado)
   },
 });
